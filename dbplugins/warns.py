47da1012620bc6412eabfdfc9680805c34b46425
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2020-09-20 19:44:32 +0530

    lint

--- dbplugins/warns.py
+++ dbplugins/warns.py
@@ -45,11 +45,17 @@ async def _(event):
     if num_warns >= limit:
         sql.reset_warns(reply_message.from_id, event.chat_id)
         if soft_warn:
-            await borg(EditBannedRequest(event.chat_id, reply_message.from_id, banned_rights))
+            await event.client(EditBannedRequest(
+                event.chat_id, reply_message.from_id, banned_rights
+            ))
             reply = "{} warnings, <u><a href='tg://user?id={}'>user</a></u> has been kicked!".format(limit, reply_message.from_id)
-            await borg(EditBannedRequest(event.chat_id, reply_message.from_id, unbanned_rights))
+            await event.client(EditBannedRequest(
+                event.chat_id, reply_message.from_id, unbanned_rights
+            ))
         else:
-            await borg(EditBannedRequest(event.chat_id, reply_message.from_id, banned_rights))
+            await event.client(EditBannedRequest(
+                event.chat_id, reply_message.from_id, banned_rights
+            ))
             reply = "{} warnings, <u><a href='tg://user?id={}'>user</a></u> has been banned!".format(limit, reply_message.from_id)
     else:
         reply = "<u><a href='tg://user?id={}'>user</a></u> has {}/{} warnings... watch out!".format(reply_message.from_id, num_warns, limit)

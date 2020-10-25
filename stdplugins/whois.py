35f7a6bf69e8103e5646dcbee5e79593a0dbca20
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2020-10-25 10:03:28 +0530

    smol fix

--- stdplugins/whois.py
+++ stdplugins/whois.py
@@ -17,7 +17,7 @@ async def _(event):
     if replied_user is None:
         await event.edit(str(error_i_a))
         return False
-    replied_user_profile_photos = await borg(GetUserPhotosRequest(
+    replied_user_profile_photos = await event.client(GetUserPhotosRequest(
         user_id=replied_user.user.id,
         offset=42,
         max_id=0,
@@ -26,7 +26,7 @@ async def _(event):
     replied_user_profile_photos_count = "NaN"
     try:
         replied_user_profile_photos_count = replied_user_profile_photos.count
-    except AttributeError as e:
+    except AttributeError:
         pass
     user_id = replied_user.user.id
     # some people have weird HTML in their names
@@ -34,6 +34,7 @@ async def _(event):
     # https://stackoverflow.com/a/5072031/4723940
     # some Deleted Accounts do not have first_name
     if first_name is not None:
+        first_name = html.escape(first_name)
         # some weird people (like me) have more than 4096 characters in their names
         first_name = first_name.replace("\u2060", "")
     last_name = replied_user.user.last_name
@@ -47,33 +48,20 @@ async def _(event):
         user_bio = html.escape(replied_user.about)
     common_chats = replied_user.common_chats_count
     try:
-        dc_id, location = get_input_location(replied_user.profile_photo)
+        dc_id, _ = get_input_location(replied_user.profile_photo)
     except Exception as e:
-        dc_id = "Need a Profile Picture to check **this**"
-        location = str(e)
+        dc_id = f"Need a Profile Picture to check {str(e)} **this**"
     caption = (
-        "ID: <code>{}</code>\n"
-        "First Name: <a href='tg://user?id={}'>{}</a>\n"
-        "🤦‍♂️ Last Name: {}\n"
-        "Bio: {}\n"
-        "DC ID: {}\n"
-        "Number of PPs: {}\n"
-        "Restricted: {}\n"
-        "Verified: {}\n"
-        "Bot: {}\n"
-        "Groups in Common: {}\n"
-    ).format(
-        user_id,
-        user_id,
-        first_name,
-        last_name,
-        user_bio,
-        dc_id,
-        replied_user_profile_photos_count,
-        replied_user.user.restricted,
-        replied_user.user.verified,
-        replied_user.user.bot,
-        common_chats
+        f"ID: <code>{user_id}</code>\n"
+        f"First Name: <a href='tg://user?id={user_id}'>{first_name}</a>\n"
+        f"🤦‍♂️ Last Name: {last_name}\n"
+        f"Bio: {user_bio}\n"
+        f"DC ID: {dc_id}\n"
+        f"Number of Pictures: {replied_user_profile_photos_count}\n"
+        f"Restricted: {replied_user.user.restricted}\n"
+        f"Verified: {replied_user.user.verified}\n"
+        f"Bot: {replied_user.user.bot}\n"
+        f"Groups in Common: {common_chats}\n"
     )
     message_id_to_reply = event.message.reply_to_msg_id
     if not message_id_to_reply:
@@ -103,13 +91,13 @@ async def get_full_user(event):
         if previous_message.forward:
             replied_user = await event.client(
                 GetFullUserRequest(
-                    previous_message.forward.from_id or previous_message.forward.channel_id
+                    previous_message.forward.sender_id or previous_message.forward.channel_id
                 )
             )
         else:
             replied_user = await event.client(
                 GetFullUserRequest(
-                    previous_message.from_id
+                    previous_message.sender_id
                 )
             )
         return replied_user, None

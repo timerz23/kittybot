c667dc7a410df774b38bdd3c8003f930063c1f6d
Yusuf_M_Thon_iD <32301831+Sunda001@users.noreply.github.com>
2021-01-21 00:12:05 +0700

    fix encoded_image values (#113)

--- stdplugins/google.py
+++ stdplugins/google.py
@@ -123,7 +123,7 @@ async def _(event):
             )
             SEARCH_URL = "{}/searchbyimage/upload".format(BASE_URL)
             multipart = {
-                "encoded_image": (downloaded_file_name, open(downloaded_file_name, "rb")),
+                "encoded_image": open(downloaded_file_name, "rb"),
                 "image_content": ""
             }
             # https://stackoverflow.com/a/28792943/4723940

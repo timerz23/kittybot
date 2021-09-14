ff1b1e8286fde97cd87d5e6a6d6fdd6d2ce27e32
sidpower <83870716+sidpower@users.noreply.github.com>
2021-09-14 15:45:14 +0530

    Update f_notification_p.py

--- dbplugins/f_notification_p.py
+++ dbplugins/f_notification_p.py
@@ -1,19 +1,3 @@
-# (c) Shrimadhav U K
-#
-# This file is part of @UniBorg
-#
-# @UniBorg is free software; you cannot redistribute it and/or modify
-# it under the terms of the GNU General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# @UniBorg is not distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-"""-_-
-"""
 from telethon import events, custom
 from telethon.tl.types import (
     Channel,

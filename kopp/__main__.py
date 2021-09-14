4387b0b47a0c2e540f12b6a2101ded307ecb6b28
sidpower <83870716+sidpower@users.noreply.github.com>
2021-09-14 15:53:34 +0530

    Update __main__.py

--- kopp/__main__.py
+++ kopp/__main__.py
@@ -1,12 +1,9 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import logging
 import os
 import sys
 from pathlib import Path
-from uniborg import Uniborg
+from kitty import Kitty
 from uniborg.storage import Storage
 from alchemysession import AlchemySessionContainer
 from telethon import events, TelegramClient
